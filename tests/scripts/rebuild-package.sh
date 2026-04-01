329635369032cf0c96cc5743cc807b8f4ff3901e
Martin Perina <mperina@redhat.com>
2026-04-01 09:53:36 +0200

    Fix incorrect repository URL when building AIB locally
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- tests/scripts/rebuild-package.sh
+++ tests/scripts/rebuild-package.sh
@@ -42,7 +42,7 @@ createrepo_c ${AIB_LOCAL_REPO}
 
 
 # TODO: Once switching to CS10 we can create repo file using dnf config-manager
-add_repo "aib-local-repo" "file:///${AIB_LOCAL_REPO}"
+add_repo "aib-local-repo" "file://${AIB_LOCAL_REPO}"
 
 # TODO: Remove following issue is fixed: https://gitlab.com/CentOS/automotive/src/automotive-image-builder/-/issues/35
 echo "excludepkgs=automotive-image-builder" >> /etc/yum.repos.d/aib-base-repo.repo
