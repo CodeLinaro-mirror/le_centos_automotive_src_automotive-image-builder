504f58aed072df4cf53c59bcbcfa41397c5f2dc2
Martin Perina <mperina@redhat.com>
2026-06-25 09:20:32 +0200

    Skip multiple invocations of prepare phase scripts in parallel runs
    Skips multiple invocations of prepare phase scripts during parallel test
    executions.
    
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- tests/scripts/init-bootc-builder.sh
+++ tests/scripts/init-bootc-builder.sh
@@ -2,6 +2,11 @@
 
 source "$(dirname ${BASH_SOURCE[0]})"/test-lib.sh
 
+if [ "$PTR_PREPARE_FINISHED" == "yes" ]; then
+    echo "Prepare phase was already executed by parallel-test-runner.sh, skipping"
+    exit 0
+fi
+
 IF_NEEDED="--if-needed"
 if [ "$REBUILD_BOOTC_BUILDER" == "yes" ]; then
     # Force the rebuild of bootc builder container
