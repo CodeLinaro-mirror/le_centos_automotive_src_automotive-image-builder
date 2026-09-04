dddf1abc0a0bb96e79f15087ec373c44c5c22eb8
Alexander Larsson <alexl@redhat.com>
2026-09-04 10:54:34 +0200

    download: Set args.mode to bootc, not image
    image_mode==image is a legacy mode and we're using bootc everywhere else.

--- aib/main.py
+++ aib/main.py
@@ -324,7 +324,7 @@ def download(args, tmpdir, runner):
     if not args.build_dir:
         raise InvalidBuildDir()
     args.out = None
-    args.mode = "image"
+    args.mode = "bootc"
     exports = []
 
     outputdir = run_osbuild(args, tmpdir, runner, exports)
