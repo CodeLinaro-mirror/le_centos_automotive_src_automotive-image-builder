825acbf6bdb54a187904bdffa1dc55cadde326ef
Martin Perina <mperina@redhat.com>
2026-09-18 11:37:37 +0200

    Fix partition size tests to verify sizes on booted image
    Previously we verified partition sizes on the image itself without
    booting the image, so we haven't found the error with incorrent mounting
    of separate /var partition. This changes fixes the test behaviour to
    verify partition size on a booted image.
    Also it was found out, that previous relative sizes of /var and /var/tmp
    in partition-relative-size test were too big comparing to the minimized
    image size, so they were decreased to fit into the image size and it was
    required to have such big allowed tolarance in the test. By decreasing
    requested relative size it was possible to get to the same 1% tolerance
    as in partition-absolute-size test.
    
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- tests/tests/partition-relative-size/run-test.sh
+++ tests/tests/partition-relative-size/run-test.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/bash -x
 
-source "$(dirname ${BASH_SOURCE[0]})"/../../scripts/test-lib.sh
+source "$(dirname "${BASH_SOURCE[0]}")"/../../scripts/test-lib.sh
 
-IMG_FILE="out.img"
+IMG_NAME="out.img"
+LOGIN_TIMEOUT=120
 
 # Update cleanup function parameters on each test artifact change
-trap 'cleanup_path "$IMG_FILE"' 'EXIT'
+trap 'cleanup_path "$IMG_NAME" ;
+      stop_vm "$VM_PID"' \
+    EXIT
 
 echo_log "Starting build..."
-build partition-relative-size.aib.yml "$NO_CTR_NAME" "$IMG_FILE"
-echo_log "Build completed, output: $IMG_FILE"
+build partition-relative-size.aib.yml "$NO_CTR_NAME" "$IMG_NAME"
+echo_log "Build completed, output: $IMG_NAME"
 
-# bootc partition sizes requires bigger tolerance
-assert_partition_relative_size "$IMG_FILE" var 0.2 0.03
-assert_partition_relative_size "$IMG_FILE" qm_var 0.1 0.03
+IMG_SIZE="$(stat -c %s "$IMG_NAME")" || fatal "FAIL: Failed to stat image file"
+EXP_VAR_SIZE="$( echo "$IMG_SIZE * 0.1 / 1" | bc)"
+EXP_VAR_QM_SIZE="$( echo "$IMG_SIZE * 0.05 / 1" | bc)"
+
+# Start the VM using the built AIB image
+VM_PID=$(run_vm "$IMG_NAME")
+
+# Wait until VM becomes available or fail fast
+if ! wait_for_vm_up "$LOGIN_TIMEOUT"; then
+    stop_vm "$VM_PID"
+    exit 1
+fi
+
+# The difference between requested relative size and real size can be 1%
+assert_block_partition_size /var "$EXP_VAR_SIZE" 1
+assert_block_partition_size /var/qm "$EXP_VAR_QM_SIZE" 1
 
 
